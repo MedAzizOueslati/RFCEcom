561ad0d86ea2441383d77a1d8a983aeb3d79e94a
MedAzizOueslati <oueslati.mohamedaziz@esprit.tn>
2024-06-06 15:10:36 +0000

    sonar*****

--- src/test/java/com/rfc/rfcecommerce/RfcEcommerceApplicationTests.java
+++ src/test/java/com/rfc/rfcecommerce/RfcEcommerceApplicationTests.java
@@ -1,13 +1,18 @@
 package com.rfc.rfcecommerce;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class RfcEcommerceApplicationTests {
 
     @Test
     void contextLoads() {
+        assertNotNull( "Le contexte de l'application n'a pas été chargé");
+
     }
 
 }
