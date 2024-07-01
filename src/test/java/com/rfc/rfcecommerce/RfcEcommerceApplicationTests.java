1527f0f30e8b5f7075be08cf6e11d82802ab1c1e
MedAzizOueslati <oueslati.mohamedaziz@esprit.tn>
2024-07-01 18:37:06 +0100

    dbpw

--- src/test/java/com/rfc/rfcecommerce/RfcEcommerceApplicationTests.java
+++ src/test/java/com/rfc/rfcecommerce/RfcEcommerceApplicationTests.java
@@ -3,11 +3,15 @@ package com.rfc.rfcecommerce;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 
 @SpringBootTest
 class RfcEcommerceApplicationTests {
+    @Autowired
+    private ApplicationContext context;
 
     @Test
     void contextLoads() {
