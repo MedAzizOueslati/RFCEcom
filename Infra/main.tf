4b8f878028a0db31db9861245aaa69e08145c18d
MedAzizOueslati <oueslati.mohamedaziz@esprit.tn>
2024-07-01 19:20:24 +0100

    infra

---
+++ Infra/main.tf
@@ -0,0 +1,52 @@
+# Définir le fournisseur Azure
+provider "azurerm" {
+  features {}
+}
+
+# Utiliser le groupe de ressources existant
+data "azurerm_resource_group" "existing" {
+  name = "RFC-Ecom"
+}
+
+# Utiliser le registre de conteneurs Azure existant
+data "azurerm_container_registry" "existing" {
+  name                = "EcomRegistr"
+  resource_group_name = data.azurerm_resource_group.existing.name
+}
+
+# Créer un cluster Kubernetes (AKS)
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "EcomCluster"
+  location            = data.azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+  dns_prefix          = "myEcomcluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1  # Utilisation de 3 nœuds
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
+  }
+
+#   role_based_access_control {
+#     enabled = true
+#   }
+
+#   tags = {
+#     environment = "Development"
+#   }
+}
+
+# Sortir la configuration kube pour se connecter au cluster AKS
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.main.kube_config_raw
+  sensitive = true
+}
