2b203864bc5d24aad6471bc8c47f17af27234b50
MedAzizOueslati <92363459+MedAzizOueslati@users.noreply.github.com>
2024-08-30 11:27:29 +0100

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,23 +1,22 @@
-# Définir le fournisseur Azure
+# Define the Azure provider
 provider "azurerm" {
   features {}
   use_msi = true
   subscription_id = "460bc14f-4852-44b0-809b-ce133bada6c4"
 }
 
-
-# Utiliser le groupe de ressources existant
+# Use the existing resource group
 data "azurerm_resource_group" "existing" {
   name = "RFC_Ecom"
 }
 
-# Utiliser le registre de conteneurs Azure existant
+# Use the existing Azure container registry
 data "azurerm_container_registry" "existing" {
   name                = "EcomRegistr"
   resource_group_name = data.azurerm_resource_group.existing.name
 }
 
-# Créer un cluster Kubernetes (AKS)
+# Create an AKS cluster
 resource "azurerm_kubernetes_cluster" "existing" {
   name                = "EcomCluster"
   location            = data.azurerm_resource_group.existing.location
@@ -26,15 +25,10 @@ resource "azurerm_kubernetes_cluster" "existing" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1  # Utilisation de 3 nœuds
+    node_count = 3  # Updated to use 3 nodes
     vm_size    = "Standard_DS2_v2"
   }
-resource "azurerm_application_gateway" "existing" {
-  name                = "ingress-appgateway"
-  resource_group_name = "MC_RFC_Ecom_EcomCluster_northeurope"
-  location            = "North Europe"
-  # Autres configurations nécessaires
-}
+
   identity {
     type = "SystemAssigned"
   }
@@ -44,13 +38,16 @@ resource "azurerm_application_gateway" "existing" {
     network_policy = "azure"
   }
 
-
-
-#   role_based_access_control {
-#     enabled = true
-#   }
-
-   tags = {
-     environment = "EcomRFC"
-   }
+  tags = {
+    environment = "EcomRFC"
+  }
 }
+
+# Define the Application Gateway resource separately
+resource "azurerm_application_gateway" "existing" {
+  name                = "ingress-appgateway"
+  resource_group_name = "MC_RFC_Ecom_EcomCluster_northeurope"
+  location            = "North Europe"
+  # Other configurations needed for the application gateway
+}
+
